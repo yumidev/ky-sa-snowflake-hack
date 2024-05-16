66e6a43903a3e056e8eac3cfbb22fc024ba42714
Yumi Ko <yumi.ko.cheers@gmail.com>
2024-05-15 23:55:47 -0600

    added news_card_detail component

--- components/news_card.py
+++ components/news_card.py
@@ -3,12 +3,16 @@ This module contains the card component where news articles are shown and given 
 """
 
 import streamlit as st
+from components.news_card_detail import card_detail
 
 CARD_HEIGHT = 300
 
-def NewsCard(column):
+def NewsCard(column, index):
     sample_img = "statics/imgs/the-new-york-times-logo.jpg" #TODO: Remove this in final version as this is just for testing
 
     card = column.container(height=CARD_HEIGHT, border=False) #creates a new container in the column
     card.image(sample_img)
+    
     card.header("Lorem Ipsum")
+    st.button('Read details', key=index, on_click=card_detail, kwargs=dict(idx=f'{index}'), help="See summary and key takeaways")
+    
