04af4dbc2d1387907743638aa79b8607e2f0865e
Yumi Ko <yumi.ko.cheers@gmail.com>
2024-05-16 00:41:46 -0600

    added component description

--- components/news_card_detail.py
+++ components/news_card_detail.py
@@ -1,5 +1,8 @@
+"""
+This module contains the news card detail component where summary and key takeaways of news article is shown.
+"""
+
 import streamlit as st
-import streamlit.components.v1 as components
 
 text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in est rutrum, imperdiet ex et, lacinia mauris. Phasellus porttitor tristique nisi non tempor. Duis lacinia porta enim non consequat. In tellus nulla, rhoncus vitae mattis non, interdum in dolor. Suspendisse potenti. Etiam id arcu posuere, pellentesque justo ut, euismod orci. Curabitur nec odio et nunc interdum aliquet nec eu magna. Integer eget risus sed leo vestibulum viverra. Cras vel sapien mi. Vestibulum pretium lacinia cursus. Fusce ullamcorper interdum elit quis fringilla.'
 
@@ -8,7 +11,7 @@ def card_detail(idx):
     # TODO: Check if summary and key takeaways of this article exists in session state.
     # If it exists, render it instead of calling snowflake arctic api
   
-    st.write(f"Read article {idx}")
+    st.write(f"Summary of article {idx}")
     sample_img = "statics/imgs/the-new-york-times-logo.jpg" #TODO: Remove this in final version as this is just for testing
     st.image(sample_img)    
     
