10c914abe7061b8f4624d989c268f2d5f11fb8a8
Josue Santana <joansanres@gmail.com>
2024-05-16 17:54:57 -0400

    Moved api handling to a more appropriate place. Created new model class

---
+++ controllers/api_handler.py
@@ -0,0 +1,120 @@
+import sys
+import os
+from dotenv import load_dotenv
+import datetime
+import requests
+import feedparser
+
+sys.path.append(".")
+from models.news_article import NewsArticle
+
+load_dotenv()
+
+rss_feeds = {
+    "bbc": "https://feeds.bbci.co.uk/news/technology/rss.xml",
+    "wsj": "https://feeds.a.dj.com/rss/RSSWSJD.xml",
+    "bloomberg": "https://feeds.bloomberg.com/technology/news.rss",
+    "forbes": "https://www.forbes.com/innovation/feed",
+    "mit": "https://cdn.technologyreview.com/stories.rss",
+    "ieee": "https://ieeetv.ieee.org/channel_rss/channel_7/rss",
+    "cnet": "https://www.cnet.com/rss/news/",
+    "verge": "https://www.theverge.com/rss/index.xml"
+}
+
+def get_articles_from_guardian():
+    DOMAIN_NAME = "https://content.guardianapis.com"
+
+    today = datetime.date.today()
+    yesterday = (today - datetime.timedelta(days=1)).isoformat()
+    one_week_ago = (today - datetime.timedelta(days=7)).isoformat()
+    tomorrow = (today + datetime.timedelta(days=1)).isoformat()
+
+
+    filter_keys = ["section", "from-date", "to-date", "api-key"]
+    filter_values = ["technology", yesterday, tomorrow, os.getenv("GUARDIAN_KEY")]
+    filter_list = [f"{a}={b}" for a, b in zip(filter_keys, filter_values)]
+    filters_concat = "&".join(filter_list)
+
+    response = requests.get(f"{DOMAIN_NAME}/search?{filters_concat}")
+    response_json = response.json()
+
+    articles = []
+
+    for result in response_json.get("response").get("results"):
+        article = NewsArticle(
+            headline=result.get("webTitle"),
+            link=result.get("webUrl"),
+            timestamp=result.get("webPublicationDate"),
+        )
+        articles.append(article)
+
+    return articles
+
+
+def get_articles_from_nyt():
+    DOMAIN_NAME = "https://api.nytimes.com"
+
+    api_key = os.getenv("NYT_KEY")
+    year = datetime.datetime.now().year
+    month = datetime.datetime.now().month
+    
+    response = requests.get(f"{DOMAIN_NAME}/svc/archive/v1/{year}/{month}.json?api-key={api_key}")
+    response_json = response.json()
+
+    articles = []
+
+    for doc in response_json.get("response").get("docs"):
+        multimedia = doc.get("multimedia", [{}])
+        if multimedia and len(multimedia) > 0:
+            img_url = multimedia[0].get("url")
+        else:
+            img_url = ""
+
+        article = NewsArticle(
+            headline=doc.get("headline").get("main"),
+            summary=doc.get("abstract"),
+            link=doc.get("web_url"),
+            timestamp=doc.get("pub_date"),
+            thumbnail_url=img_url
+        )
+        articles.append(article)
+    
+    return articles
+
+
+def get_rss_feed_data(url):
+    feed = feedparser.parse(url)
+
+    articles = []
+
+    for entry in feed.entries:
+        article = NewsArticle(
+            headline=entry.get("title"),
+            summary=entry.get("summary"),
+            link=entry.get("link"),
+            timestamp=entry.get("published"),
+            thumbnail_url=entry.get("media_thumbnail", [{}])[0].get("url")
+        )
+        articles.append(article)
+    
+    return articles
+
+
+def get_scraped_articles():
+    ## TODO: Complete this implementation
+    import pandas as pd
+    from bs4 import BeautifulSoup
+    from selenium import webdriver
+    from selenium.webdriver.firefox.options import Options
+    from selenium.webdriver.firefox.service import Service as FFService
+    from selenium.webdriver.common.by import By
+
+    options = Options()
+    options.add_argument("--headless")
+    service = FFService(executable_path="/snap/bin/geckodriver")
+    driver = webdriver.Firefox(options=options, service=service)
+    driver.get('https://www.bbc.com/innovation/artificial-intelligence')
+    blog_titles = driver.find_elements(By.CSS_SELECTOR, 'h2.bvDsJq')
+    for title in blog_titles:
+        print(title.text)
+    driver.quit()  # closing the browser
--- models/news_article.py
+++ models/news_article.py
@@ -1,3 +1,12 @@
 """
 This module contains data associated with news articles. 
 """
+
+class NewsArticle():
+    def __init__(self, headline, summary="", link="", timestamp="", thumbnail_url=""):
+        self.headline = headline
+        self.summary = summary
+        self.link = link
+        self.timestamp = timestamp
+        self.thumbnail_url = thumbnail_url
+
