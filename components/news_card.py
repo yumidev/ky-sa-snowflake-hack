add0454fbaad74e59e4f6921d1d12233b9df9acd
Yumi Ko <yumi.ko.cheers@gmail.com>
2024-05-20 12:22:00 -0700

    show thumbnail in news card

--- components/news_card.py
+++ components/news_card.py
@@ -8,11 +8,10 @@ from components.news_card_detail import card_detail
 CARD_HEIGHT = 400
 
 def NewsCard(column, index, article):
-    sample_img = "statics/imgs/the-new-york-times-logo.jpg" #TODO: Remove this in final version as this is just for testing
-
-    card = column.container(height=CARD_HEIGHT, border=False) #creates a new container in the column
-    card.image(sample_img)
-
     headline = article.get("headline")
+    thumbnail_url = article.get("thumbnail_url")
+    
+    card = column.container(height=CARD_HEIGHT, border=False) #creates a new container in the column
+    card.markdown(f"![Image - {headline}]({thumbnail_url})")
     card.markdown(f"### {headline}")
     st.button('Read summary', key=index, on_click=card_detail, kwargs=dict(idx=f'{index}'), help="See summary and key takeaways")
