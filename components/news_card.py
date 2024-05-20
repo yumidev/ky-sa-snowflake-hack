68858401ee951077d2dab8a317d970aac07c9484
Yumi Ko <yumi.ko.cheers@gmail.com>
2024-05-20 16:31:05 -0700

    use article data in detail view

--- components/news_card.py
+++ components/news_card.py
@@ -5,13 +5,20 @@ This module contains the card component where news articles are shown and given 
 import streamlit as st
 from components.news_card_detail import card_detail
 
-CARD_HEIGHT = 400
+CARD_HEIGHT = 350
 
 def NewsCard(column, index, article):
     headline = article.get("headline")
     thumbnail_url = article.get("thumbnail_url")
     
+    detail_data = dict(headline=headline, thumbnail_url=thumbnail_url)
+    
     card = column.container(height=CARD_HEIGHT, border=False) #creates a new container in the column
     card.markdown(f"![Image - {headline}]({thumbnail_url})")
     card.markdown(f"### {headline}")
-    st.button('Read summary', key=index, on_click=card_detail, kwargs=dict(idx=f'{index}'), help="See summary and key takeaways")
+    st.button(
+        'Read summary',
+        key=index,
+        on_click=card_detail,
+        kwargs=detail_data,
+        help="See summary and key takeaways")
