616807b28db28c3f725ca13982df26ea5dc1d7cc
Yumi Ko <yumi.ko.cheers@gmail.com>
2024-05-21 00:24:48 -0700

    fetch articles on change filter

--- pages/Main.py
+++ pages/Main.py
@@ -17,6 +17,7 @@ def show_page():
     Navbar()
     
     st.title("Bellman AI")
+
     if "articles" not in st.session_state:
         with st.spinner("Loading your AI news..."):
             curated_articles = load_most_recent_articles()
