f5bc1a467025d274ef90a94f3e813eda75da9387
Yumi Ko <yumi.ko.cheers@gmail.com>
2024-05-23 10:58:26 -0700

    fixed one row issue, removed unused files, and added new line

--- controllers/curator.py
+++
@@ -1,3 +0,0 @@
-"""
-This module is in charge of narrowing down and organizing the articles to be shown to the user.
-"""
--- controllers/text_generator.py
+++
@@ -1,3 +0,0 @@
-"""
-This module is in charge of generating text for the NewsCard, whether it is a summary or an explanation.
-"""
